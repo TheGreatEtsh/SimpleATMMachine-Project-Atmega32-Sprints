5a60b0dbf199a635e7a4ecdb7e18645e3b6d3de7
momen <momenhassan@gmail.com>
2023-05-01 17:12:32 +0200

    uart

--- SimpleATMMachine/CARD/MCAL/uart/uart.c
+++ SimpleATMMachine/CARD/MCAL/uart/uart.c
@@ -4,3 +4,45 @@
  * Created: 5/1/2023 2:57:57 PM
  *  Author: Ahmed Hesham
  */ 
+
+#include "uart.h"
+
+void UART_Init(void)
+{
+	// Set BaudRate -> 9600/12MhZ
+	UBRRL = 51;
+	UBRRH = 0;
+	// Set Frame Format -> 8 data, 1 stop, No Parity
+	SET_BIT(UCSRC,UCSZ0);
+	SET_BIT(UCSRC,UCSZ1);
+	SET_BIT(UCSRC,URSEL);
+	
+	// Enable RX and TX
+	UCSRB = 0x18;
+	SET_BIT(UCSRB,RXEN);
+	SET_BIT(UCSRB,TXEN);
+
+}
+
+void UART_SendChar(u8 data)
+{
+	// Wait until transmission Register Empty
+	
+	while(GET_BIT(UCSRA,UDRE) == 0x00);
+	UDR = data;
+}
+
+u8 UART_GetChar(void)
+{
+	u8 Result;
+	// Wait until Reception Complete
+
+	while(	GET_BIT(UCSRA,RXC) == 0x00);
+	Result = UDR;
+
+	/* Clear Flag */
+	SET_BIT(UCSRA,RXC);
+	return Result;
+
+}
+
--- SimpleATMMachine/CARD/MCAL/uart/uart.h
+++ SimpleATMMachine/CARD/MCAL/uart/uart.h
@@ -1,4 +1,4 @@
-/*
+x/*
  * uart.h
  *
  * Created: 5/1/2023 2:58:11 PM
@@ -8,6 +8,18 @@
 
 #ifndef UART_H_
 #define UART_H_
+
+
+#include "../dio/dio.h"
+#include "../registers.h"
+#include "../../SERVICE/bit_math.h"
+
+void UART_Init(void);
+
+void UART_SendChar(u8 data);
+
+u8 UART_GetChar(void);
+
 
 
 
