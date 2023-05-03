dcba699e694615965451f3d4dbab2495af6309ba
Ahmed atef <ahmedaatf15@gmail.com>
2023-05-03 05:40:47 +0300

    add_fun_ee

--- SimpleATMMachine/CARD/ECUAL/eeprom/eeprom.h
+++ SimpleATMMachine/CARD/ECUAL/eeprom/eeprom.h
@@ -13,5 +13,6 @@
 void eeprom_init(void);
 void eeprom_WriteByte(u8 *data ,u16 u16_addr);
 void eeprom_ReadByte (u8 *data ,u16 u16_addr ); 
-
+u8 eeprom_WriteString (u16 u16_Saddr, u8 *u8_Pdata);
+u8 eeprom_ReadString (u16 u16_Saddr, u8 *u8_Pdata);
 #endif /* EEPROM_H_ */
