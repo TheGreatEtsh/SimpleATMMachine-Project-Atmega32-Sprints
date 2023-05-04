a44807a560a5947a3a7c64426804a3675e32dcde
TheGreatEtsh <ahmedhesham606@icloud.com>
2023-05-04 16:17:36 +0300

    Updating Card

--- SimpleATMMachine/CARD/APP/app.c
+++ SimpleATMMachine/CARD/APP/app.c
@@ -235,48 +235,6 @@ void APP_superLoop(void)
 	}
 }
 
-
-
- 
- //  void I2C_TEST (void)
- //  {
- // 	 u8 data;
- // 	 if (I2C_start()==EVENT_OK)
- // 	 {
- // 		 LCD_WriteString("start I2C_1");
- //
- // 		 if ( I2C_address_select(0x10,read)==SLA_R_ACK_SENT)
- // 		 {
- // 			 if(I2C_data_rw(&data,read,NACK)==DATA_READ_NACK_SENT)
- // 			 {
- // 				 LCD_SetCursor(1,0);
- // 				 LCD_WriteString("stop I2C_1");
- // 				 I2C_stop();
- // 			 }
- // 		 }
- // 	 }
- //
- // 	 if (I2C_start()==EVENT_OK)
- // 	 {
- // 		 if ( I2C_address_select(0x10,write)==SLA_W_ACK_SENT)
- // 		 {        LCD_Clear();
- // 			 LCD_WriteString("start I2C_2");
- // 			 if(I2C_data_rw(&data,write,ACK)==DATA_WRITE_ACK_SENT)
- // 			 {
- // 				 LCD_SetCursor(1,0);
- // 				 LCD_WriteString("stop  I2C_2");
- // 				 I2C_stop();
- // 			 }
- // 		 }
- // 	 }
- //
- // 	 while(1)
- // 	 {
- //
- // 	 }
- //  }
-
-
 void APP_testingEEPROM (void)
 {
 	TIMER_init(TIMER_2);
